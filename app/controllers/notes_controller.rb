a915f70795767e5d08364c4c92ff6f03e741a5d8
Sarah McAlear <sarah.mcalear@gmail.com>
2015-03-07 13:26:14 -0500

    adds feature for user to edit notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,12 +1,29 @@
 class NotesController < ApplicationController
 
-  def create
-    Note.create note_params
-    @notes = User.find(session[:user_id]).destinations
+  def index
+    user = current_user
+    destination = user.destinations.find(params[:destination_id])
+    notes = destination.notes
     respond_to do |format|
       format.html
-      format.json { render json: @notes.last.notes.last }
+      format.json { render json: notes.sort_by { |note| note.created_at } }
     end
+  end
+
+  def create
+    Note.create note_params
+    user = current_user
+    @destinations = user.destinations
+    respond_to do |format|
+      format.html
+      format.json { render json: @destinations.last.notes.last }
+    end
+  end
+
+  def update
+    @note = Note.find(params[:id])
+    @note.update note_params
+    render json: @note
   end
 
   def destroy
