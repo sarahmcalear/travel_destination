38f29cca6292efe572be09d88a7e4c553d64ce64
Sarah McAlear <sarah.mcalear@gmail.com>
2015-03-08 19:33:36 -0400

    adds styling and organizes JS files

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,14 +1,22 @@
 class DestinationsController < ApplicationController
   include DestinationHelper
+  include ApplicationHelper
 
   def index
     @user = User.find(session[:user_id])
     @note = Note.new
     @destinations = User.find(session[:user_id]).destinations
-    data = retrieve_info_from_google(params[:location]) if params[:location]
+    params[:location] ? (desired_destination = params[:location]) : (desired_destination = choose_random_destination)
+    google_data = retrieve_info_from_google(desired_destination)
+    panoramio_pics = retrieve_panoramio_photos(desired_destination)
+    # TODO: optimize this for speed.
+    # wolfram_data = retrieve_info_from_wolfram(desired_destination)
     respond_to do |format|
       format.html
-      format.json { render json: data }
+      # TODO: optimize wolfram datafor speed
+      # format.json { render json: {google_data: google_data, wolfram_data: wolfram_data} }
+      format.json { render json: {google_data: google_data, panoramio_pics: panoramio_pics, searchLocation: desired_destination} }
+      # format.json { render json: {panoramio_pics: panoramio_pics, searchLocation: desired_destination} }
     end
   end
 
@@ -21,7 +29,6 @@ class DestinationsController < ApplicationController
     end
     @destinations = User.find(session[:user_id]).destinations
     render json: @destinations
-
   end
 
   def destroy
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,16 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+# make sure that fonts inside the vendor file work and are precompiled for Heroku in Production
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+
+# Adding Webfonts to the Asset Pipeline
+Rails.application.config.assets.precompile << Proc.new { |path|
+  if path =~ /\.(eot|svg|ttf|woff)\z/
+    true
+  end
+}
+
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts")
